d55bf129d58909c98d12b0e1254761b083af8d50
Edgar Juan Guercio <jguercio@live.com.ar>
2023-09-27 20:16:31 -0300

    EditProduct calcular precio si cambia de proveedor

--- src/controllers/Products/editProductController.js
+++ src/controllers/Products/editProductController.js
@@ -1,13 +1,23 @@
-const { Products } = require('../../db.js');
+const { Products, Providers } = require('../../db.js');
+const calculatePrice = require("../../utils/calculatePrice.js");
+const IVA = 21;
 
-const editProductController = async (id, product) => {
-    // Buscar el producto por su ID en la base de datos
+const editProductController = async (id, newProductData) => {
+    // Buscar el producto a editar
     const productToEdit = await Products.findByPk(id);
+    if (!productToEdit) throw Error("Producto no encontrado");
 
-    if (!productToEdit) throw Error("PRODUCTO NO ENCONTRADO");
+    // Buscar proveedor correspondiente
+    const provider = await Providers.findOne({ where: { name: newProductData.patent } });
+    if (!provider) throw Error('Proveedor no encontrado');
 
-    await productToEdit.update(product);
-    return productToEdit;
+    //Actualizar producto con nuevos datos
+    await productToEdit.update({
+        ...newProductData,
+        price: calculatePrice(newProductData.price, provider.discount, IVA, provider.markup),
+    });
+
+    return productToEdit.dataValues;
 };
 
 module.exports = editProductController;
