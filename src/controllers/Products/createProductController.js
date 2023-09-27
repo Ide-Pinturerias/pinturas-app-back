98a43cf84e580310beef7033542f14e3ef0d6cc5
Edgar Juan Guercio <jguercio@live.com.ar>
2023-09-27 03:08:59 -0300

    crear producto con provedor realcionado y calculo de precio final

--- src/controllers/Products/createProductController.js
+++ src/controllers/Products/createProductController.js
@@ -1,11 +1,19 @@
-const { Products } = require('../../db.js');
+const { Products, Providers } = require('../../db.js');
+const calculatePrice = require("../../utils/calculatePrice.js");
+const IVA = 21;
 
 const createProductController = async (product) => {
-    return Products.findOrCreate({
-        where: {
-            ...product,
-        },
+    const provider = await Providers.findOne({ where: { name: product.patent } });
+
+    if (!provider) throw Error('Provider not found');
+
+    const productCreated = await Products.create({
+        ...product, providerId: provider.id
     });
+    await productCreated.update({
+        price: calculatePrice(productCreated.price, provider.discount, IVA, provider.markup),
+    });
+    return productCreated.dataValues;
 };
 
 module.exports = createProductController;
