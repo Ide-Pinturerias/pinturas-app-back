da1eff0c32469c0a5d19737dde834862726b0e29
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-20 20:01:03 -0300

    proteccion Providers

--- src/handlers/Providers/getProvidersHandler.js
+++ src/handlers/Providers/getProvidersHandler.js
@@ -1,22 +1,8 @@
 const { ProvidersControllers } = require('../../controllers');
 const { getProviders } = ProvidersControllers;
-const decodedToken = require("../../services/decodedJwt");
 
 const getProvidersHandler = async (req, res) => {
-
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({
-        error: "Falta el token de autorización"
-    });
-
     try {
-        const authorization = decodedToken(token);
-        if (authorization.rol !== "admin") {
-
-            return res.status(403).json({
-                error: "No cuentas con los permisos para esta sección"
-            });
-        }
         return res.status(200).json(await getProviders());
     } catch (error) {
         return res.status(500).json({ error: error.message });
