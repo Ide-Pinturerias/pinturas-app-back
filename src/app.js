1ced492f3c51f0fe4bd798e67def8f64c7116beb
jamerrq <jamerrq@gmail.com>
2024-01-12 16:39:38 -0500

    [SNYK] security fixes with snyk

--- src/app.js
+++ src/app.js
@@ -1,19 +1,37 @@
 const express = require('express');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index.js');
-const { rateLimiter } = require('./middlewares/');
+const routes = require('#ROUTES');
 const path = require('path');
+require('dotenv').config();
+const { ALLOWED_ORIGINS, SESSION_SECRET } = process.env;
+const { csrf } = require('#MIDDLEWARES');
 
+// Init Express Server
 const server = express();
+
+// Session (cookies) configuration
+const session = require('express-session');
+server.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+// CSRF protection
+server.use(cookieParser());
+server.use(csrf);
+
+// Helmet
+server.use(helmet());
 // Proxy configuration
 const trustProxyFn = (/* ip */) => {
   // Por ahora, confiamos en todas las conexiones
   return true;
 };
 server.set('trust proxy', trustProxyFn);
-server.use(rateLimiter);
 // Middleware para capturar la dirección
 // IP del encabezado X - Forwarded - For cuando esté presente
 server.use((req, res, next) => {
@@ -26,11 +44,10 @@ server.name = 'API';
 // MIDDLEWARES
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
+server.use((_req, res, next) => {
   // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', ALLOWED_ORIGINS);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -46,12 +63,19 @@ server.use(express.static(path.join(__dirname, 'public')));
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 server.use('/', routes);
 
+// Disable some properties
+server.disable('x-powered-by');
+
 // Error catching endware.
 server.use((err, _req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+  if (err) {
+    console.error(err);
+    return res.status(status).send({
+      success: false,
+      error: 'Internal Server Error'
+    });
+  }
   if (next) next();
 });
 
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,17 +1,21 @@
 const { Router } = require('express');
-const { UsersHandlers } = require('../handlers/');
-const auth = require('../middlewares/auth');
+const { UsersHandlers } = require('#HANDLERS');
+const { auth, createRateLimiter } = require('#MIDDLEWARES');
 
 const router = Router();
 
+// Para limitar el número de peticiones por IP para /login y /register
+const loginRateLimiter = createRateLimiter(0, 1, 5);
+const registerRateLimiter = createRateLimiter(0, 24, 5);
+
 // 1. POST /users/register
-router.post('/register', UsersHandlers.createUser);
+router.post('/register', registerRateLimiter, UsersHandlers.createUser);
 
 // 2. GET /users
 router.get('/', UsersHandlers.getUsers);
 
 // 3. POST /users/login
-router.post('/login', UsersHandlers.loginUsers);
+router.post('/login', loginRateLimiter, UsersHandlers.loginUsers);
 
 // 4. DELETE /users/:id
 router.delete('/:id', UsersHandlers.deleteUser);
