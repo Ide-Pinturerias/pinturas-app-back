2d739ba7230310ec49653ba8cef8d9b7a1f73644
Edgar Juan Guercio <jguercio@live.com.ar>
2024-01-22 19:41:41 -0300

    FEAT funcion para formatear los productos del carrito para la logica de create order

--- src/controllers/Orders/createOrderByCartIdController.js
+++ src/controllers/Orders/createOrderByCartIdController.js
@@ -1,24 +1,49 @@
 const { Carts } = require('#DB_CONNECTION');
 const createOrder = require('./createOrderController');
 const {
-  MISSING_PARAMS_ERROR,
-  CART_NOT_FOUND_ERROR
+    MISSING_PARAMS_ERROR,
+    CART_NOT_FOUND_ERROR
 } = require('#ERRORS');
 
+const formatProductsCart = (stringProducts) => {
+    try {
+        const arrayProducts = JSON.parse(stringProducts);
+        const objectProducts = arrayProducts.reduce((acc, product) => {
+            // Extraer valores de las claves "id" y "quantity" del product
+            const { id, quantity } = product;
+
+            // Agregar el valor de "id" al array "ids"
+            acc.ids.push(Number(id));
+
+            // Agregar el valor de "quantity" al array "qus"
+            acc.qus.push(quantity);
+
+            return acc;
+        }, { ids: [], qus: [] });
+
+        return objectProducts;
+    } catch (error) {
+        //to-do manejo de errores con formato jamer
+        console.error(error);
+    }
+};
+
 const creatOrderByCartIdController = async ({ idCart }) => {
-  if (!idCart) throw new MISSING_PARAMS_ERROR('Missing params');
+    if (!idCart) throw new MISSING_PARAMS_ERROR('Missing params');
 
-  const cart = await Carts.findOne({ where: { idCart } });
+    const cart = await Carts.findOne({ where: { idCart } });
 
-  if (!cart) {
-    throw new CART_NOT_FOUND_ERROR(`Cart with id ${idCart} not found`);
-  }
+    if (!cart) {
+        throw new CART_NOT_FOUND_ERROR(`Cart with id ${idCart} not found`);
+    }
 
-  const { products, userId } = cart;
+    const { products, userId } = cart;
 
-  const order = await createOrder({ products, idUser: userId });
+    const productsFormated = formatProductsCart(products);
 
-  return order;
+    const order = createOrder({ products: productsFormated, idUser: userId });
+
+    return order;
 };
 
 module.exports = creatOrderByCartIdController;
--- src/controllers/Orders/createOrderController.js
+++ src/controllers/Orders/createOrderController.js
@@ -1,61 +1,61 @@
 const { Orders, Users, Products } = require('#DB_CONNECTION');
 const {
-  MISSING_PARAMS_ERROR,
-  USER_NOT_FOUND_ERROR,
-  BAD_FORMAT_JSON_ERROR,
-  PRODUCT_NOT_FOUND_ERROR
+    MISSING_PARAMS_ERROR,
+    USER_NOT_FOUND_ERROR,
+    BAD_FORMAT_JSON_ERROR,
+    PRODUCT_NOT_FOUND_ERROR
 } = require('#ERRORS');
 
 const getTotal = async (products) => {
-  // "products": "{\"ids\":[1,2,3],\"qus\":[1,2,3]}"
-  try {
-    const parsedProducts = JSON.parse(products);
-    const { ids, qus } = parsedProducts;
-    if (ids.length !== qus.length) {
-      throw new BAD_FORMAT_JSON_ERROR('Bad format json');
-    }
-    const productsToGet = (await Products.findAll({ where: { idProduct: ids } })).map((product) => product.dataValues);
-    // if some product is not found in the database, throw an error
-    productsToGet.forEach((product) => {
-      if (!product) {
-        throw new PRODUCT_NOT_FOUND_ERROR(`Product with id ${product.id} not found`);
-      }
-    });
-    let total = 0;
-    productsToGet.forEach((product, index) => {
-      const q = qus[index];
-      if (isNaN(q) || q < 0) {
+    // "products": "{\"ids\":[1,2,3],\"qus\":[1,2,3]}"
+    try {
+        const { ids, qus } = products;
+        if (ids.length !== qus.length) {
+            throw new BAD_FORMAT_JSON_ERROR('Bad format json');
+        }
+        const productsToGet = (await Products.findAll({ where: { idProduct: ids } })).map((product) => product.dataValues);
+        // if some product is not found in the database, throw an error
+        productsToGet.forEach((product) => {
+            if (!product) {
+                throw new PRODUCT_NOT_FOUND_ERROR(`Product with id ${product.id} not found`);
+            }
+        });
+        let total = 0;
+        productsToGet.forEach((product, index) => {
+            const q = qus[index];
+            if (isNaN(q) || q < 0) {
+                throw new BAD_FORMAT_JSON_ERROR('Bad format json');
+            }
+            total += product.price * qus[index];
+        });
+        return total;
+    } catch (error) {
+        console.error(`Error in getTotal: ${error.message}`);
+        console.error(error);
         throw new BAD_FORMAT_JSON_ERROR('Bad format json');
-      }
-      total += product.price * qus[index];
-    });
-    return total;
-  } catch (error) {
-    console.error(`Error in getTotal: ${error.message}`)
-    console.error(error);
-    throw new BAD_FORMAT_JSON_ERROR('Bad format json');
-  }
+    }
 };
 
 const createOrderController = async ({ products, idUser }) => {
-  if (!products || !idUser) throw new MISSING_PARAMS_ERROR('Missing params');
+    console.log(products, idUser);
+    if (!products || !idUser) throw new MISSING_PARAMS_ERROR('Missing params');
 
-  const userOrder = await Users.findByPk(idUser);
+    const userOrder = await Users.findByPk(idUser);
 
-  if (!userOrder) {
-    throw new USER_NOT_FOUND_ERROR(`User with id ${idUser} not found`);
-  }
+    if (!userOrder) {
+        throw new USER_NOT_FOUND_ERROR(`User with id ${idUser} not found`);
+    }
 
-  const total = await getTotal(products);
+    const total = await getTotal(products);
+    console.log(total);
+    const order = await Orders.create({
+        products: JSON.stringify(products),
+        total
+    });
 
-  const order = await Orders.create({
-    products,
-    total
-  });
+    await userOrder.addOrder(order);
 
-  await userOrder.addOrder(order);
-
-  return order;
+    return order;
 };
 
 module.exports = createOrderController;
