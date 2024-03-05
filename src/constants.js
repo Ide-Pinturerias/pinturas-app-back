ed1cef0f29e41bf4c4b935a7bbaaf1ea0521c743
Edgar Juan Guercio <jguercio@live.com.ar>
2024-03-05 19:01:21 -0300

    EDIT const front

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 module.exports = {
-  DEPLOY_URL_BASE: 'https://pinturas-app-back-production.up.railway.app/',
-  FRONT_URL_BASE: 'https://pinturas-app-front-production.up.railway.app/',
+  FRONT_URL_BASE: 'https://pinturas-app-front-production.up.railway.app',
   IVA: 21,
   PASSWORD_REGEX: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*_-]{8,}$/
 };
